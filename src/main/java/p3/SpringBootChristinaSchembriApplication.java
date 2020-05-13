bf9e2fb3b8659b3e3e9509f42627a292fab95477
Christina <cls18@student.fdu.edu>
2020-05-13 18:59:00 -0400

    initial commit

---
+++ src/main/java/p3/SpringBootChristinaSchembriApplication.java
@@ -0,0 +1,13 @@
+package p3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChristinaSchembriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChristinaSchembriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/p3/SpringBootChristinaSchembriApplicationTests.java
@@ -0,0 +1,13 @@
+package p3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChristinaSchembriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
