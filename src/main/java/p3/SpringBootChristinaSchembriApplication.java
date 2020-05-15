d14ef134c046bdaaa5a954417a0512a801029562
Christina <cls18@student.fdu.edu>
2020-05-15 19:26:28 -0400

    updates

--- src/main/java/p3/SpringBootChristinaSchembriApplication.java
+++ src/main/java/p3/SpringBootChristinaSchembriApplication.java
@@ -2,8 +2,10 @@ package p3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootChristinaSchembriApplication {
 
 	public static void main(String[] args) {
