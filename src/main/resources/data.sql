67e1b8e3b72b052143d375df5c3ec765b4975ee1
Christina <cls18@student.fdu.edu>
2020-05-15 21:55:16 -0400

    some fixes of templates

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,9 +5,9 @@ INSERT INTO cat(name, type) VALUES('black', 'tekir');
 -- EOF initialize (insert) tables used by jdbcTemplate in h2 db
 
 -- BEF initialize (insert) tables used by jpa in h2 db
-INSERT INTO dog(id, name, type) VALUES(1000, 'white', 'maltiese');
-INSERT INTO dog(id, name, type) VALUES(1001, 'yellow', 'chihuahua');
-INSERT INTO dog(id, name, type) VALUES(1002, 'black', 'germanShepherd');
-INSERT INTO dog(id, name, type) VALUES(1003, 'brown', 'yorki');
-INSERT INTO dog(id, name, type) VALUES(1004, 'milky', 'kangal');
+--INSERT INTO dog(id, name, type) VALUES(1000, 'white', 'maltiese');
+--INSERT INTO dog(id, name, type) VALUES(1001, 'yellow', 'chihuahua');
+--INSERT INTO dog(id, name, type) VALUES(1002, 'black', 'germanShepherd');
+--INSERT INTO dog(id, name, type) VALUES(1003, 'brown', 'yorki');
+--INSERT INTO dog(id, name, type) VALUES(1004, 'milky', 'kangal');
 -- EOF initialize (insert) tables used by jpa in h2 db
